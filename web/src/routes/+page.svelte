c8fe2f325033eb97e3e6a86380d0acf2969c5b14
Josef Müller <46693545+am9zZWY@users.noreply.github.com>
2024-12-22 23:12:31 +0100

    Wording update

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,9 +1,10 @@
 <script>
 	import Hero from '../lib/components/Hero.svelte';
+	import Uploader from "$lib/components/Uploader.svelte";
 </script>
 
 <svelte:head>
-    <title>Darf Vermieter das? - KI-gestützte Mietrechtsanalyse</title>
+    <title>Darf Vermieter das? – KI-gestützte Mietrechtsanalyse</title>
 </svelte:head>
 
 <Hero />
