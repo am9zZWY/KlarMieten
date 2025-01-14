f66817161bd3f8827ed67717a3534cc0b17deae4
Josef Müller <46693545+am9zZWY@users.noreply.github.com>
2025-01-14 22:06:48 +0100

    Update svelte config

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,5 +1,29 @@
-import adapter from "svelte-adapter-bun";
+import {default as bunAdapter} from "svelte-adapter-bun";
+import {default as autoAdapter} from '@sveltejs/adapter-auto';
 import {vitePreprocess} from '@sveltejs/vite-plugin-svelte';
+
+const adapterType = process.env.ADAPTER || "auto"
+
+const _bunAdapter = bunAdapter({
+	out: "build",
+	assets: true,
+	development: process.env.NODE_ENV === "development",
+	// precompress: true,
+	precompress: {
+		brotli: true,
+		gzip: true,
+		files: ["htm", "html"],
+	},
+	dynamic_origin: true,
+	xff_depth: 1,
+})
+
+const _autoAdapter = autoAdapter()
+
+let adapter = _autoAdapter
+if (adapterType === "bun") {
+	adapter = _bunAdapter
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,19 +35,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			out: "build",
-			assets: true,
-			development: process.env.NODE_ENV === "development",
-			// precompress: true,
-			precompress: {
-				brotli: true,
-				gzip: true,
-				files: ["htm", "html"],
-			},
-			dynamic_origin: true,
-			xff_depth: 1,
-		}),
+		adapter,
 	}
 };
 
