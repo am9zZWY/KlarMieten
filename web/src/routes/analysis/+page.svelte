8d34e9443ef195fcb6ab802a987bdea26bba39bc
Josef Müller <46693545+am9zZWY@users.noreply.github.com>
2025-02-13 00:24:22 +0100

    Remove web

--- web/src/routes/analysis/+page.svelte
+++
@@ -1,9 +0,0 @@
-<h1>Vertragsanalyse</h1>
-<section>
-    <!-- Placeholder for image -->
-    <div class="image-placeholder"></div>
-</section>
-
-<svelte:head>
-    <title>Darf Vermieter das? | Analyse</title>
-</svelte:head>
