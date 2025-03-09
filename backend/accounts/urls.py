94d70b38b058d8f2ff7eb3e67a0f9642d182cfcd
Josef Müller <46693545+am9zZWY@users.noreply.github.com>
2025-03-10 00:02:53 +0100

    Add Bootstrap and update most UI pages

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,9 +1,13 @@
+from django.contrib.auth.views import LoginView
 from django.urls import path
 
 from .views import SignUpView
 
-
-
 urlpatterns = [
-    path("signup/", SignUpView.as_view(), name="signup")
+    path(
+        "accounts/login",
+        LoginView.as_view(template_name="registration/login.html"),
+        name="login",
+    ),
+    path("accounts/signup", SignUpView.as_view(), name="signup"),
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,10 +1,24 @@
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from .forms import LoginForm, RegisterForm
 
-from .forms import RegisterForm
 
+def login_view(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data["username"],
+                password=form.cleaned_data["password"],
+            )
+            if user:
+                login(request, user)
+                return redirect("home")
+        return render(request, "registration/login.html", {"form": form})
+    return render(request, "registration/login.html", {"form": LoginForm()})
 
-# Create your views here.
 
 class SignUpView(CreateView):
     form_class = RegisterForm
