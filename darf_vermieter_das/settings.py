56c47e2d53a808c9e64ee0b22a62e4c539bbfd78
Josef Müller <46693545+am9zZWY@users.noreply.github.com>
2025-03-19 19:39:03 +0100

    Simplify database settings in settings.py by removing Vercel condition

--- darf_vermieter_das/settings.py
+++ darf_vermieter_das/settings.py
@@ -111,24 +111,24 @@ WSGI_APPLICATION = "darf_vermieter_das.wsgi.application"
 
 # DATABASE SETTINGS
 # ------------------------------------------------------------------------------
-if IS_VERCEL:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.getenv("POSTGRES_DB"),
-            "USER": os.getenv("POSTGRES_USER"),
-            "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-            "HOST": os.getenv("POSTGRES_HOST"),
-            "PORT": os.getenv("POSTGRES_PORT", "5432"),
-        }
+# if IS_VERCEL:
+#     DATABASES = {
+#         "default": {
+#             "ENGINE": "django.db.backends.postgresql",
+#             "NAME": os.getenv("POSTGRES_DB"),
+#             "USER": os.getenv("POSTGRES_USER"),
+#             "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+#             "HOST": os.getenv("POSTGRES_HOST"),
+#             "PORT": os.getenv("POSTGRES_PORT", "5432"),
+#         }
+#     }
+# else:
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+}
 
 # MEDIA FILES
 # ------------------------------------------------------------------------------
