e78bbb2a3473c1ac5551e5574cc54905ad60ce9c
Josef Müller <46693545+am9zZWY@users.noreply.github.com>
2025-03-19 20:37:01 +0100

    Refactor build process by removing static file collection and updating vercel.json for static file handling

--- build_files.sh
+++ build_files.sh
@@ -8,7 +8,4 @@ pip3 install -r requirements.txt
 echo "### Migrating database"
 python3 manage.py migrate --noinput
 
-echo "### Collecting static files"
-python3 manage.py collectstatic --noinput --clear
-
 echo "Build completed successfully"
